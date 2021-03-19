fceed34d6857d74d5ba2c4b6e555fc47091b37a5
amr33mohamd <amr2010mohamd2010@gmail.com>
2021-03-19 11:48:26 +0200

    typed added

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -7,4 +7,8 @@ $(document).ready(function() {
         $(this).prev().css('left', pop + '%').text(prgsVal);
         $(this).val(prgsVal);
     });
+    var typed = new Typed('#typed', {
+      strings: ['', 'Your technology partner for innovative and impactful digital solutions.'],
+      typeSpeed:40
+    });
 });
